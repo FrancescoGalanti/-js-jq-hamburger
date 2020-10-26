ca4386513796602e07d8400e79c7b8b131ded9a7
FrancescoGalanti <galanti_francesco>
2020-10-26 15:43:20 +0100

    prima funzione fatta

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /** jquery
 **
 **/
+
+
+
+var hamburger = $('.hamburger-menu');
+var bars = $("header .header-right > a");
+var close $("")
+
+bars.click( function(){
+   hamburger.addClass("active");
+});
